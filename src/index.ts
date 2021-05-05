7991d040b7088d99c0b9021cbbb6f5632d76326b
David Rautureau <david.rautureau@sonarsource.com>
2021-05-05 20:14:58 +0200

    Add Hotspot

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection(
+{
+  host:'localhost',
+  user: "admin",
+  database: "project",
+  password: "mypassword", // sensitive
+  multipleStatements: true
+});
+
+connection.connect();
+
 export function fibonacci(num: number): number | null {
   if (num < 0) {
     return null;
