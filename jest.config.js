2524c2ea322e54e032db97e284a607f636c647ac
7PH <benjamin.raymond@sonarsource.com>
2024-04-16 10:50:41 +0200

    NO-JIRA Upgrade dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-  globals: {
-    "ts-jest": {
-      skipBabel: true,
-    },
-  },
   moduleFileExtensions: ["js", "ts"],
   testResultsProcessor: "jest-sonar-reporter",
   transform: {
-    "^.+\\.ts$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
+    "^.+\\.ts$": "ts-jest",
   },
   testMatch: ["<rootDir>/tests/**/*.test.ts"],
+  preset: "ts-jest",
 };
